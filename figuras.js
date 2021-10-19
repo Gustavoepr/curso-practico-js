2fa2505f37de58135d8f9ac7439a2a2d5b34be2c
Gustavo Pereira <gustavoepr1110vv@gmail.com>
2021-10-19 08:40:05 -0300

    se agregaron todas las operaciones matematicas necesarias para cada caso

--- figuras.js
+++ figuras.js
@@ -24,7 +24,7 @@ function diametro(radio){
     return radio * 2;
 }
 function circunferencia(radio){
-    var diametro = diametro(radio);
+    var diametro = radio * 2;
     return diametro * PI
 }
 function areaciculo(radio){
@@ -41,6 +41,14 @@ function CalcularPerimetroCuadrado(){
     alert(perimetro)
 }
 
+function CalcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = Number(input.value);
+
+    const areadelcuadrado = areacuadrado(value);
+    alert(areadelcuadrado);
+}
+
 function CalcularPerimetroTriangulo(){
     const input1 = document.getElementById("Lado-1");
     const value1 = Number(input1.value);
@@ -53,4 +61,31 @@ function CalcularPerimetroTriangulo(){
 
     var perimetro = perimetriangulo(value1, value2, value3);
     alert(perimetro)
+}
+
+function CalcularAreaTriangulo(){
+    const input1 =document.getElementById("Base-Area");
+    const value1 = Number(input1.value);
+
+    const input2 = document.getElementById("Altura");
+    const value2 = Number(input2.value)
+
+    var arTriangulo = areatiangulo(value1, value2);
+    alert(arTriangulo)
+}
+
+function CalcularPerimetroCirculo(){
+    const input = document.getElementById("InputRadio");
+    const value = Number(input.value);
+
+    var perCirculo = circunferencia(value);
+    alert(perCirculo);
+}
+
+function CalcularAreaCiculo(){
+    const input = document.getElementById("InputRadio");
+    const value = Number(input.value);
+
+    var arCirculo = areaciculo(value);
+    alert(arCirculo);
 }
