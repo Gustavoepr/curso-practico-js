3bfe23b6c3e2b72227d2fb812a55c91e32b8dbd1
Gustavo Pereira <gustavoepr1110vv@gmail.com>
2021-10-19 07:49:29 -0300

    Se agrego el calculo de perimetro del cuadrado y tiangulo interactuando con el HTML

--- figuras.js
+++ figuras.js
@@ -10,7 +10,7 @@ console.groupEnd();
 //tiangulo
 console.group();
 function perimetriangulo(lado1, lado2, base){
-    return lado1 + lado2 + base;
+    return pertriangulo = lado1 + lado2 + base;
 }
 function areatiangulo(base, altura){
     return (base * altura) / 2;
@@ -37,6 +37,20 @@ function CalcularPerimetroCuadrado(){
     const input = document.getElementById("InputCuadrado");
     const value = input.value;
     
-    perimetro = perimetrocuadrado(value);
+    var perimetro = perimetrocuadrado(value);
+    alert(perimetro)
+}
+
+function CalcularPerimetroTriangulo(){
+    const input1 = document.getElementById("Lado-1");
+    const value1 = Number(input1.value);
+
+    const input2 = document.getElementById("Lado-2");
+    const value2 = Number(input2.value);
+
+    const input3 = document.getElementById("Base");
+    const value3 = Number(input3.value);
+
+    var perimetro = perimetriangulo(value1, value2, value3);
     alert(perimetro)
 }
